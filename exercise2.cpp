1b3107f094a2b095b6d2f915b718ed854ce8d902
Ian Berigan <IanBerigan17@gmail.com>
2019-10-08 16:06:06 +0100

    Add files via upload

---
+++ exercise2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int sum=0;
+    for(int i=1;i<9;i++)
+    {
+        if(i%2==0;)
+        {
+            sum=sum+i;
+        }
+
+    }
+    cout<<sum<<endl;
+    return 0;
+    
+}
